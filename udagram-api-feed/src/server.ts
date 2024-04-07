657499632c043a652e482a5733ebfb62e8e3e1a9
Hữu Chinh <khleson79929@gmail.com>
2024-04-07 15:25:40 +0700

    Init commit

---
+++ udagram-api-feed/src/server.ts
@@ -0,0 +1,45 @@
+import cors from 'cors';
+import express from 'express';
+import {sequelize} from './sequelize';
+
+import {IndexRouter} from './controllers/v0/index.router';
+
+import bodyParser from 'body-parser';
+import {config} from './config/config';
+import {V0_FEED_MODELS} from './controllers/v0/model.index';
+
+
+(async () => {
+  await sequelize.addModels(V0_FEED_MODELS);
+
+  console.debug("Initialize database connection...");
+  await sequelize.sync();
+
+  const app = express();
+  const port = process.env.PORT || 8081;
+
+  app.use(bodyParser.json());
+
+  // We set the CORS origin to * so that we don't need to
+  // worry about the complexities of CORS this lesson. It's
+  // something that will be covered in the next course.
+  app.use(cors({
+    allowedHeaders: [
+      'Origin', 'X-Requested-With',
+      'Content-Type', 'Accept',
+      'X-Access-Token', 'Authorization',
+    ],
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    preflightContinue: true,
+    origin: '*',
+  }));
+
+  app.use('/api/v0/', IndexRouter);
+
+
+  // Start the Server
+  app.listen( port, () => {
+    console.log( `server running ${port}` );
+    console.log( `press CTRL+C to stop server` );
+  } );
+})();
---
+++ udagram-api-user/src/sequelize.ts
@@ -0,0 +1,21 @@
+import {Sequelize} from 'sequelize-typescript';
+import {config} from './config/config';
+
+
+export const sequelize = new Sequelize({
+  'username': config.username,
+  'password': config.password,
+  'database': config.database,
+  'host': config.host,
+
+  'dialect': config.dialect,
+  'storage': ':memory:',
+  dialectOptions: {
+    ssl: {
+      require: true, 
+      rejectUnauthorized: false // This line will fix new error
+    }
+  },
+});
+console.log("config:: ", config);
+
