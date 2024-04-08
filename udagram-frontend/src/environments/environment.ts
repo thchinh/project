a7aa7b58d7d3bfe2b617814a0d303810b0ed9371
Hữu Chinh <khleson79929@gmail.com>
2024-04-08 23:02:31 +0700

    update env for FE call

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a6f5751a089024696b074776e6fcafa5-1453097735.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
